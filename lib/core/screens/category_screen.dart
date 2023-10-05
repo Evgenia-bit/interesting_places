9c6394ae0edddf456dce5626cfb1da3252079c2c
Evgenia-bit <ev.mo4alina@yandex.ru>
2023-10-05 18:00:18 +0300

    fixes

--- lib/core/screens/category_screen.dart
+++ lib/core/screens/category_screen.dart
@@ -1,10 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:interesting_places/core/data/category.dart';
 import 'package:interesting_places/core/themes/app_colors.dart';
 import 'package:interesting_places/core/widgets/app_button.dart';
-import 'package:interesting_places/features/category/presentation/bloc/category_bloc.dart';
-
 @RoutePage()
 class CategoryScreen extends StatefulWidget {
   const CategoryScreen({super.key});
@@ -14,12 +12,11 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
-  int? activeCategoryId;
+  Category? activeCategory;
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
-    final category = context.watch<CategoryBloc>().state.categoryList;
-
+    
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -40,14 +37,14 @@ class _CategoryScreenState extends State<CategoryScreen> {
         child: Column(
           children: [
             const SizedBox(height: 16),
-            ...category
+            ...Category.values
                 .map(
                   (c) => _CategoryListItem(
                     name: c.name,
-                    isActive: c.id == activeCategoryId,
+                    isActive: c == activeCategory,
                     onPressed: () {
                       setState(() {
-                        activeCategoryId = c.id;
+                        activeCategory = c;
                       });
                     },
                   ),
@@ -56,7 +53,7 @@ class _CategoryScreenState extends State<CategoryScreen> {
             const Spacer(),
             AppButton(
               text: 'Сохранить',
-              onPressed: activeCategoryId != null
+              onPressed: activeCategory != null
                   ? () {
                       context.router.pop();
                     }
